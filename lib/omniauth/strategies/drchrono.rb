d9ee5df2d2d781e693002ed10cac40a467cdc49b
John Nadeau <johnnnadeau@gmail.com>
2015-01-14 00:34:42 -0500

    username as uid, extra raw_info, doctor infomation in info

--- lib/omniauth/strategies/drchrono.rb
+++ lib/omniauth/strategies/drchrono.rb
@@ -11,6 +11,24 @@ module OmniAuth
         :authorize_url => 'https://drchrono.com/o/authorize',
         :token_url => 'https://drchrono.com/o/token'
       }
+
+      uid { raw_info['username'] }
+
+      info do
+        { :doctor => doctor }
+      end
+
+      extra do
+        { :raw_info => raw_info }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('users/current').parsed
+      end
+
+      def doctor
+        @doctor ||= access_token.get(raw_info['doctor']).parsed
+      end
     end
   end
 end
