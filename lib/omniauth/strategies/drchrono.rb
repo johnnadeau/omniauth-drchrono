029016207988e624461f9b19139f4cc5c7f7ede4
John Nadeau <johnnnadeau@gmail.com>
2015-01-13 22:54:54 -0500

    Revert "adding code from example..."
    This reverts commit b56df8d4182110f7fc11c000c70bdecf9b931f9e.

--- lib/omniauth/strategies/drchrono.rb
+++ lib/omniauth/strategies/drchrono.rb
@@ -10,25 +10,6 @@ module OmniAuth
         :authorize_url => '/o/authorize',
         :token_url => '/o/token'
       }
-
-      uid{ raw_info['id'] }
-
-      info do
-        {
-          :name => raw_info['name'],
-          :email => raw_info['email']
-        }
-      end
-
-      extra do
-        {
-          'raw_info' => raw_info
-        }
-      end
-
-      def raw_info
-        @raw_info ||= access_token.get('/me').parsed
-      end
     end
   end
 end
